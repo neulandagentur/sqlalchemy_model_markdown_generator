f298133066b80b5d386ceafb687fe778692e0641
Jan-Markus Langer <hallo@janmarkuslanger.de>
2020-01-24 10:01:30 +0100

    Add tablename to test model

--- test_model.py
+++ test_model.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
 Model = declarative_base(name='Model')
 
-class TestModel(Model)
+class TestModel(Model):
+    __tablename__ = 'test_model'
+    
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     email = Column(String, unique=True)
